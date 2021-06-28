589e0d0198537473de18edbf123fc816dcc86552
Soňa Husáková <bibicd13@gmail.com>
2021-06-28 19:38:47 +0200

    parse input

--- ui.js
+++ ui.js
@@ -11,5 +11,41 @@ function updateUI() {
 }
 
 function executeInput() {
-    console.log(document.getElementById("command_input").value)
+    let commandIinput = document.getElementById("command_input").value;
+    if (commandIinput.length != 4) {
+        console.warn('Invalid input length');
+        return;
+    };
+    let inputTeamIndex = parseInt(commandIinput.slice(0,1),10)-1;
+    if (isNaN(inputTeamIndex) || inputTeamIndex < 0 || inputTeamIndex > teamCount-1) {
+        console.warn('Invalid input, team does not exist');
+        return;
+    };
+    let inputXCoords = commandIinput.slice(1,2);
+    let inputYCoords = commandIinput.slice(2,3);
+    inputXCoords = parseCoords(inputXCoords);
+    inputYCoords = parseCoords(inputYCoords);
+    if (isNaN(inputXCoords) || isNaN(inputYCoords)) {
+        console.warn('Invalid input coordinates');
+        return;
+    };
+    switch (commandIinput.slice(-1)) {
+        case '+':
+            console.log('+');
+            break;
+        case '-':
+            console.log('-');
+            break;
+        default:
+            console.log('Invalid input, last character must be + or -');
+            return;
+    };
+}
+
+function parseCoords(coords) {
+    if (coords.toLowerCase() == 'a') {
+        return 10;
+    } else {
+        return parseInt(coords);
+    };
 }
